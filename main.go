3ba8ceb56966a89db44f144604ea5c01c5e6e4e2
ulises-marmolejo-r <ulises@reservamos.com>
2025-09-19 17:18:14 -0600

    Chapter 2.2 Web Application Basics.
    Initialize basic web application with handler, router, and server setup
    
    - Added `home` handler to respond with "Hello from Snippetbox"
    - Configured `http.NewServeMux()` and registered route "/"
    - Started HTTP server on port 4000 using `http.ListenAndServe`

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const port = "4123"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox Home Page!"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Print a log message to say that the server is starting.
+	log.Println("starting server on :" + port)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit. Note
+	// that any error returned by http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
